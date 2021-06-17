f320c914fe68b2fc70418381022b86fe57c0dc47
Fernancode548 <fmontesinosestrada@gmail.com>
2021-06-17 18:59:12 -0500

    devalt170621

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 function obtenerFrases() {
-    const url = 'https://quote-garden.herokuapp.com/api/v3/quotes/random'
+    const url = 'https://quote-garden.herokuapp.com/api/v3/quotes'
     fetch(url)
     .then( resultado => resultado.json())
     .then( datos => {
         const containerInfoFrases = datos.data
-        //console.log(containerInfoFrases);
+        console.log(containerInfoFrases);
 
         containerInfoFrases.forEach( datosFrase => {
-            // console.log(datosFrase.quoteAuthor);
-            // console.log(datosFrase.quoteGenre);
-            // console.log(datosFrase.quoteText);
-            document.getElementById('autor').textContent = datosFrase.quoteAuthor
-            document.getElementById('genero').textContent = datosFrase.quoteGenre
-            document.getElementById('frase').textContent = datosFrase.quoteText
+            const { quoteAuthor, quoteGenre, quoteText} = datosFrase;
+            
+            const autor = document.createElement('H1');
+            autor.textContent = `Autor: ${quoteAuthor}`;
+            autor.classList.add('author');
+
+            const genero  = document.createElement('H2');
+            genero.textContent = quoteGenre;
+            genero.classList.add('gener');
+
+            const frase = document.createElement('H3');
+            frase.textContent = quoteText;
+            frase.classList.add('frase');
+
+            // const contenedor = document.querySelector('.container');
+            const fraseDiv = document.createElement('DIV');
+            fraseDiv.classList.add('card-Principal');
+
+            fraseDiv.appendChild(autor);
+            fraseDiv.appendChild(genero);
+            fraseDiv.appendChild(frase);
+
+            const contenedorFrases = document.querySelector('.container');
+            contenedorFrases.appendChild(fraseDiv);
+            
+
+           
         })
     })
 }
